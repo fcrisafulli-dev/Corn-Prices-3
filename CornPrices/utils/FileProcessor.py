d759f931a80dddc6eca547f0e10d5e48fdebe3cc
fcrisafulli <francescocrisafulli3@gmail.com>
2020-07-23 11:43:24 -0400

    cleaned up game path

--- CornPrices/utils/FileProcessor.py
+++ CornPrices/utils/FileProcessor.py
@@ -4,20 +4,18 @@ from pathlib import Path
 from CornPrices.classes import CornPricesGame
 
 __root = Path(".").absolute()
+__game_path = __root / "storage" / "game" / f"game.cornprices"
 
 def initialize_game():
-    game_path = __root / "storage" / "game" / f"game.cornprices"
-    with open(game_path, 'wb+') as game_file:
+    with open(__game_path, 'wb+') as game_file:
         pickle.dump(CornPricesGame(), game_file)
 
 def save_game(game_instance: CornPricesGame):
-    game_path = __root / "storage" / "game" / f"game.cornprices"
-    with open(game_path, 'wb+') as game_file:
+    with open(__game_path, 'wb+') as game_file:
         pickle.dump(game_instance, game_file)
 
 def load_game():
-    game_path = __root / "storage" / "game" / f"game.cornprices"
-    with open(game_path, 'rb') as game_file:
+    with open(__game_path, 'rb') as game_file:
         game_class = pickle.load(game_file)
     return game_class
 
@@ -28,7 +26,7 @@ def check_environment():
         makedirs(__root / "storage" / "game")
         print("Created ./storage/game")
 
-    if not path.exists(__root / "storage" / "game" / "game.cornprices"):
+    if not path.exists(__game_path):
         initialize_game()
         print("Created ./storage/game/game.cornprices")
     
